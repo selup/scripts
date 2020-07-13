3447fd0a77a0b4d036dac80a09ac5471cea7dea4
bpatout <bpatout@gmail.com>
2020-07-14 00:37:48 +0200

    Update trk_script_v1-0.js

--- trk_script_v1-0.js
+++ trk_script_v1-0.js
@@ -11,10 +11,10 @@ var url = new URL(trk_url);
 var search_params = url.searchParams;
 // add "topic" parameter
 search_params.set('trk_param', search_params);
-search_params.set('trk_url', trk_url);
+//search_params.set('trk_url', trk_url);
 search_params.set('trk_referer', trk_referer);
 search_params.set('trk_path', trk_path);
 url.search = search_params.toString();
 var new_url = url.toString();
 console.log(new_url);
-window.history.pushState({},{},new_url);
+window.history.pushState({},{},new_url); 
