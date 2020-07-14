827b673a506bc983e82bf70c4162991053f5da89
bpatout <bpatout@gmail.com>
2020-07-14 18:25:16 +0200

    Update trk_script_v1-0.js

--- trk_script_v1-0.js
+++ trk_script_v1-0.js
@@ -44,37 +44,46 @@ if (conf_pagetype == "submit")
 	//////////
 	// path //
 	//////////
-	// set url param
 	var var_submit = window.location.pathname;
-	search_params.set('trk_subm_path', var_submit);
-	// set input value
-	var elt_input = $('[data-custom-type="trk_subm_path"]');
-	elt_input.attr('value',var_submit);
-	// set cookie
-	document.cookie = 'trk_subm_path='+var_submit;
+	if( var_submit != "")
+	{
+		// set url param
+		search_params.set('trk_subm_path', var_submit);
+		// set input value
+		var elt_input = $('[data-custom-type="trk_subm_path"]');
+		elt_input.attr('value',var_submit);
+		// set cookie
+		document.cookie = 'trk_subm_path='+var_submit;
+	}
 	
 	///////////
 	//  Name //
 	///////////
-	// set url param
-	search_params.set('trk_subm_name', conf_name);
-	// set input value
-	var elt_input = $('[data-custom-type="trk_subm_name"]');
-	elt_input.attr('value',conf_name);
-	// set cookie
-	document.cookie = 'trk_subm_name='+conf_name;
+	if( conf_name != "")
+	{
+		// set url param
+		search_params.set('trk_subm_name', conf_name);
+		// set input value
+		var elt_input = $('[data-custom-type="trk_subm_name"]');
+		elt_input.attr('value',conf_name);
+		// set cookie
+		document.cookie = 'trk_subm_name='+conf_name;
+	}
 	
 	
 	////////////
 	// Option //
 	////////////
-	// set url param
-	search_params.set('trk_subm_opt', conf_option);
-	// set input value
-	var elt_input = $('[data-custom-type="trk_subm_opt"]');
-	elt_input.attr('value',conf_option);
-	// set cookie
-	document.cookie = 'trk_subm_opt='+conf_option;
+	if( conf_option != "")
+	{
+		// set url param
+		search_params.set('trk_subm_opt', conf_option);
+		// set input value
+		var elt_input = $('[data-custom-type="trk_subm_opt"]');
+		elt_input.attr('value',conf_option);
+		// set cookie
+		document.cookie = 'trk_subm_opt='+conf_option;
+	}
 }
 
 /*****************/
@@ -85,21 +94,23 @@ else if(conf_pagetype == "landing")
 	//////////
 	// path //
 	//////////
-	// set url param
 	var var_landing = window.location.pathname;
-	search_params.set('trk_lp_path', var_landing);
-	// set input value
-	var elt_input = $('[data-custom-type="trk_lp_path"]');
-	elt_input.attr('value',var_landing);
-	// set cookie
-	document.cookie = 'trk_lp_path='+var_landing;
+	if( var_landing != "")
+	{		
+		// set url param
+		search_params.set('trk_lp_path', var_landing);
+		// set input value
+		var elt_input = $('[data-custom-type="trk_lp_path"]');
+		elt_input.attr('value',var_landing);
+		// set cookie
+		document.cookie = 'trk_lp_path='+var_landing;
+	}
 	
 	///////////
 	//  Name //
 	///////////
 	if( conf_name != "")
 	{
-		console.log("not null");
 		// set url param
 		search_params.set('trk_lp_name', conf_name);
 		// set input value
@@ -113,13 +124,16 @@ else if(conf_pagetype == "landing")
 	////////////
 	// Option //
 	////////////
-	// set url param
-	search_params.set('trk_lp_opt', conf_option);
-	// set input value
-	var elt_input = $('[data-custom-type="trk_lp_opt"]');
-	elt_input.attr('value',conf_option);
-	// set cookie
-	document.cookie = 'trk_lp_opt='+conf_option;
+	if( conf_option != "")
+	{	
+		// set url param
+		search_params.set('trk_lp_opt', conf_option);
+		// set input value
+		var elt_input = $('[data-custom-type="trk_lp_opt"]');
+		elt_input.attr('value',conf_option);
+		// set cookie
+		document.cookie = 'trk_lp_opt='+conf_option;
+	}
 
 }
 
