249f9db10a2e5c591af032eaa5399d16f95c13ac
bpatout <bpatout@gmail.com>
2020-07-15 22:04:54 +0200

    Update trk_script_v1-0.js

--- trk_script_v1-0.js
+++ trk_script_v1-0.js
@@ -1,7 +1,9 @@
-var elt_input = $('[data-title="row-trk"]');
-//elt_input.style.display = "none";
-elt_input.attr('style','display:""');
 
+// Print forms (for debug only) - caution, all page with this form will be show
+/*
+var elt_input = $('[data-title="row-trk"]');
+elt_input.attr('style','display:""');
+*/
 
 function getCookie(cname) {
   var name = cname + "=";
