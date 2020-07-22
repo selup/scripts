83bee2f8524c8ee44c176816d6e55026bd40e7df
bpatout <bpatout@gmail.com>
2020-07-23 01:47:21 +0200

    Update trkac.js

--- trkac.js
+++ trkac.js
@@ -2,14 +2,16 @@ function trk_ac_update_contact(contact)
 {
 	console.log(contact.email);
 	
- 	const Http = new XMLHttpRequest();
-	const url='https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec';
+ 	var Http = new XMLHttpRequest();
+	var url='https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec'+'?email='+contact.email;
 	Http.open("GET", url);
 	Http.send();
 
+/*
 	Http.onreadystatechange = (e) => 
 	{
 	  console.log(Http.responseText)
 	}
+	*/
 }
 
