0a42107fbb3fc85a52442fa5c6ef78e32b96fea8
bpatout <bpatout@gmail.com>
2020-07-24 21:10:16 +0200

    Create trktool.js

---
+++ trktool.js
@@ -0,0 +1,6 @@
+async function encodeSHA256(message) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(message);
+  const hash = await crypto.subtle.digest('SHA-256', data);
+  return hash;
+}
