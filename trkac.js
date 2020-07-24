5cc8ef8cc9ab3b58fd432f788e3ca272ac282abc
bpatout <bpatout@gmail.com>
2020-07-24 18:48:12 +0200

    Update trkac.js

--- trkac.js
+++ trkac.js
@@ -5,11 +5,9 @@ function trk_ac_update_contact(contact)
 		var urlWebApp = "https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec";
 		var bodyData = [];
 		bodyData.push ("https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec"+"?email="+contact.email) ;
-		bodyData.push
-		( 
-			if( (contact.utm_source != "") && (contact.utm_source != undefined) )  { ['utm_source='+contact.utm_source] }
-			//if( (contact.utm_campaign != "") && (contact.utm_campaign != undefined) )  { ['utm_campaign='+contact.utm_campaign] }
-		);	
+		
+		if( (contact.utm_source != "") && (contact.utm_source != undefined) ) {bodyData.push({ ['utm_source='+contact.utm_source] }) ;}
+
 		//var UrlToCall = bodyData.map(function(el){el[1] = encodeURIComponent(el[1]); return el.join('=')}).join('&');
 		var UrlToCall = bodyData.join('&');
 		
