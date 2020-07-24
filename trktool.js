1b14d45826f61f94772194289334bef6b04d3e67
bpatout <bpatout@gmail.com>
2020-07-24 21:40:29 +0200

    Update trktool.js

--- trktool.js
+++ trktool.js
@@ -1,6 +1,13 @@
+/*
 async function encodeSHA256(message) {
   const encoder = new TextEncoder();
   const data = encoder.encode(message);
   const hash = await crypto.subtle.digest('SHA-256', data);
+  return hash;
+}
+*/
+
+function encodeSHA256(message) {
+  var hash = crypto.subtle.digest('SHA-256', message);
   return hash;
 }
