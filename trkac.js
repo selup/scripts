059c90031b5809834311f8a08ca8cf35c1478fd2
bpatout <bpatout@gmail.com>
2020-07-24 18:28:46 +0200

    Update trkac.js

--- trkac.js
+++ trkac.js
@@ -1,11 +1,20 @@
 function trk_ac_update_contact(contact)
 {
-	console.log(contact.email);
-	
- 	var Http = new XMLHttpRequest();
-	var url='https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec'+'?email='+contact.email;
-	Http.open("GET", url);
-	Http.send();
-
+	if( (concat.email != "") && (concat.email != undefined) )
+	{
+		var urlWebApp = "https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec";
+		var bodyData = [];
+		bodyData.push ("https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec"+"?email="+contact.email) ;
+		bodyData.push
+		( 
+			['utm_source' , contact.utm_source]
+		);	
+		var UrlToCall = bodyData.map(function(el){el[1] = encodeURIComponent(el[1]); return el.join('=')}).join('&');
+		
+		var Http = new XMLHttpRequest();
+		//var url='https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec'+'?email='+contact.email;
+		Http.open("GET", UrlToCall);
+		Http.send();
+	}
 }
 
