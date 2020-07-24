918f3b949ad7d2fa58ae8d0c8b82cd70a8b9713a
bpatout <bpatout@gmail.com>
2020-07-24 19:56:35 +0200

    Update trkac.js

--- trkac.js
+++ trkac.js
@@ -6,14 +6,14 @@ function trk_ac_update_contact(contact)
 		
 		
 		var bodyData = [];
-		bodyData.push (urlWebApp+"?email="+contact.email) ;
-		if( (contact.path != "") && (contact.path != undefined) ) { bodyData.push(['path='+contact.path]); }
+		bodyData.push (urlWebApp+"?email="+encodeURIComponent(contact.email)) ;
+		if( (contact.path != "") && (contact.path != undefined) ) { bodyData.push(['path='+encodeURIComponent(contact.path)]); }
 		
-		if( (contact.utm_source != "") && (contact.utm_source != undefined) ) { bodyData.push(['utm_source='+contact.utm_source]); }
-		if( (contact.utm_campaign != "") && (contact.utm_campaign != undefined) ) { bodyData.push(['utm_campaign='+contact.utm_campaign]); }
-		if( (contact.utm_term != "") && (contact.utm_term != undefined) ) { bodyData.push(['utm_term='+contact.utm_term]); }
-		if( (contact.utm_medium != "") && (contact.utm_medium != undefined) ) { bodyData.push(['utm_medium='+contact.utm_medium]); }
-		if( (contact.utm_content != "") && (contact.utm_content != undefined) ) { bodyData.push(['utm_content='+contact.utm_content]); }
+		if( (contact.utm_source != "") && (contact.utm_source != undefined) ) { bodyData.push(['utm_source='+encodeURIComponent(contact.utm_source)]); }
+		if( (contact.utm_campaign != "") && (contact.utm_campaign != undefined) ) { bodyData.push(['utm_campaign='+encodeURIComponent(contact.utm_campaign)]); }
+		if( (contact.utm_term != "") && (contact.utm_term != undefined) ) { bodyData.push(['utm_term='+encodeURIComponent(contact.utm_term)]); }
+		if( (contact.utm_medium != "") && (contact.utm_medium != undefined) ) { bodyData.push(['utm_medium='+encodeURIComponent(contact.utm_medium)]); }
+		if( (contact.utm_content != "") && (contact.utm_content != undefined) ) { bodyData.push(['utm_content='+encodeURIComponent(contact.utm_content)]); }
 		
 		var UrlToCall = bodyData.join('&');	
 		console.log("url is:"+UrlToCall);
