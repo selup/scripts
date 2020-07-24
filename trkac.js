17b696e0e1c189cbc5dc675c00ae057c409459c2
bpatout <bpatout@gmail.com>
2020-07-24 18:53:06 +0200

    Update trkac.js

--- trkac.js
+++ trkac.js
@@ -3,16 +3,18 @@ function trk_ac_update_contact(contact)
 	if( (contact.email != "") && (contact.email != undefined) )
 	{
 		var urlWebApp = "https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec";
+		
+		
 		var bodyData = [];
 		bodyData.push (urlWebApp+"?email="+contact.email) ;
-		
 		if( (contact.utm_source != "") && (contact.utm_source != undefined) ) { bodyData.push(['utm_source='+contact.utm_source]); }
-
-		//var UrlToCall = bodyData.map(function(el){el[1] = encodeURIComponent(el[1]); return el.join('=')}).join('&');
-		var UrlToCall = bodyData.join('&');
+		if( (contact.utm_campaign != "") && (contact.utm_campaign != undefined) ) { bodyData.push(['utm_campaign='+contact.utm_campaign]); }
+		
+		var UrlToCall = bodyData.join('&');	
+		
+		
 		
 		var Http = new XMLHttpRequest();
-		//var url='https://script.google.com/macros/s/AKfycbwDKQdFDCCCKNy47Zw8q7pz2edltXtYBHStj9e7GuwNZb-7jUq2/exec'+'?email='+contact.email;
 		Http.open("GET", UrlToCall);
 		Http.send();
 	}
