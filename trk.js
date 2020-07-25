8e10717dd88c0504af0122ab80b975a1d5e21063
bpatout <bpatout@gmail.com>
2020-07-26 01:22:18 +0200

    Update trk.js

--- trk.js
+++ trk.js
@@ -252,22 +252,6 @@ var trk =
 
 
 
-if (window.requestIdleCallback) {
-   requestIdleCallback(function () {
-       Fingerprint2.get(function (components) {
-         console.log(components) // an array of components: {key: ..., value: ...}
-       })
-   })
-} else {
-   setTimeout(function () {
-       Fingerprint2.get(function (components) {
-         console.log(components) // an array of components: {key: ..., value: ...}
-       })  
-   }, 500)
-}
-
-
-
 
 
 // Load sha256.js in special mode for GTM
