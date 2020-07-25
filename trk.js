748feeac8da2d446407525316a95d17a15236fb8
bpatout <bpatout@gmail.com>
2020-07-26 00:15:02 +0200

    Update trk.js

--- trk.js
+++ trk.js
@@ -203,7 +203,7 @@ function trk_SetEvent(str_evt,property)
 */
 
 function trk_GetEmail() {
-   if (TRKDBG_FUNCIN) { console.log("=>" + arguments.callee.name "()"); }
+   if (TRKDBG_FUNCIN) { console.log("=>" + arguments.callee.name + "()"); }
    var value_encoded = localStorage.getItem('trk_eml_enc');
    if (value_encoded) { var value_decoded = window.atob(value_encoded); }
    if (TRKDBG_GETEMAIL) {
