693b15c998b486d031a482287c3342c97fa1ba5e
bpatout <bpatout@gmail.com>
2020-07-26 02:22:24 +0200

    Update trk.js

--- trk.js
+++ trk.js
@@ -303,7 +303,7 @@ new Fingerprint2.get(function(result, components) {
 
 function processFingerprint(data) {
    console.log(data.fingerprint);
-   var values = data.fingerprint.map;
+   var values = data.fingerprint.map(data.fingerprint.value);
    var murmur = Fingerprint2.x64hash128(values.join(''), 31);   
    console.log(murmur);
 }
