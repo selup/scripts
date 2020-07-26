008acb0fbbd6f8d5703d768396ee9980be46169d
bpatout <bpatout@gmail.com>
2020-07-26 13:36:37 +0200

    Update trk.js

--- trk.js
+++ trk.js
@@ -49,16 +49,26 @@ function trk_WriteEmailOnLocalStorageOnEvent()   // create ...
 }
 
 
+function trk_LoadSHA256script()
+{
+   // Load sha256.js in special mode for GTM
+   (function () {
+      if (TRKDBG_FUNCIN) { console.log("=>load sha256.js()"); }
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://selup.github.io/scripts/sha256.js';
+      script.setAttribute('integrity', 'sha384-IT/pLh1Rncx9vLpihvEzJBju0K02/VtM4auFugUU965Ufj6XRyv+aQivyTngKzNN');
+      script.setAttribute('crossorigin', 'anonymous');
+      document.getElementsByTagName('head')[0].appendChild(script);
+   })();
+}
+
 
 
 
 /////////////
 // Main () //
 /////////////
-
-//dynamicallyLoadScript("https://selup.github.io/scripts/trk-fct.js"); 
-
-console.log(trk_GetDateString);
 
 var trk =
 {
@@ -75,17 +85,7 @@ trk_SetFingerPrintAsync();
 trk_WriteEmailOnLocalStorageOnEvent();
 
 
-// Load sha256.js in special mode for GTM
-(function () {
-   if (TRKDBG_FUNCIN) { console.log("=>load sha256.js()"); }
-   var script = document.createElement('script');
-   script.type = 'text/javascript';
-   script.src = 'https://selup.github.io/scripts/sha256.js';
-   script.setAttribute('integrity', 'sha384-IT/pLh1Rncx9vLpihvEzJBju0K02/VtM4auFugUU965Ufj6XRyv+aQivyTngKzNN');
-   script.setAttribute('crossorigin', 'anonymous');
-   document.getElementsByTagName('head')[0].appendChild(script);
-})();
-
+trk_LoadSHA256script();
 
 
 
