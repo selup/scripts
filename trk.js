190e65a958ea7e8613349808d566d21f3f4f6056
bpatout <bpatout@gmail.com>
2020-07-26 15:13:49 +0200

    Update trk.js

--- trk.js
+++ trk.js
@@ -12,7 +12,19 @@ var TRKDBG_VERBOSE = true;
 /////////////
 // Main () //
 /////////////
-trk_LoadSHA256script();
+//trk_LoadSHA256script();
+
+   // Load sha256.js in special mode for GTM
+   (function () {
+      if (TRKDBG_FUNCIN) { console.log("=>load sha256.js()"); }
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://selup.github.io/scripts/sha256.js';
+      script.setAttribute('integrity', 'sha384-IT/pLh1Rncx9vLpihvEzJBju0K02/VtM4auFugUU965Ufj6XRyv+aQivyTngKzNN');
+      script.setAttribute('crossorigin', 'anonymous');
+      document.getElementsByTagName('head')[0].appendChild(script);
+   })();
+   
 
 trk_SetFingerPrintAsync();
 
